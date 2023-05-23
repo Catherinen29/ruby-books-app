e12e5e2b5a1db484041fca4b138c99d5091575d9
Catherine Nevin <cat_nevin@hotmail.com>
2023-05-23 21:48:09 +0100

    Create Book model and see

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_23_201934) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_23_203023) do
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -20,4 +20,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_23_201934) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "books", force: :cascade do |t|
+    t.integer "author_id", null: false
+    t.string "title"
+    t.string "genre"
+    t.date "published"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_books_on_author_id"
+  end
+
+  add_foreign_key "books", "authors"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,19 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 
-Author.create(first_name: 'Banana', last_name: 'Yoshimoto', number_of_books: 12, author_birthdate: 1964-07-24)
-Author.create(first_name: 'Haruki', last_name: 'Murakami', number_of_books: 28, author_birthdate: 1949-01-12)
-Author.create(first_name: 'Hanya', last_name: 'Yanagihara', number_of_books: 3, author_birthdate: 1974-01-01 )
-Author.create(first_name: 'Mieko', last_name: 'Kawakami', number_of_books: 13, author_birthdate: 1976-8-29 )
-Author.create(first_name: 'Elena', last_name: 'Ferrante', number_of_books: 12, author_birthdate: 1943-04-05)
+# Author.create(first_name: 'Banana', last_name: 'Yoshimoto', number_of_books: 12, author_birthdate: 1964-07-24)
+# Author.create(first_name: 'Haruki', last_name: 'Murakami', number_of_books: 28, author_birthdate: 1949-01-12)
+# Author.create(first_name: 'Hanya', last_name: 'Yanagihara', number_of_books: 3, author_birthdate: 1974-01-01 )
+# Author.create(first_name: 'Mieko', last_name: 'Kawakami', number_of_books: 13, author_birthdate: 1976-8-29 )
+# Author.create(first_name: 'Elena', last_name: 'Ferrante', number_of_books: 12, author_birthdate: 1943-04-05)
+
+
+Book.create(author_id: 1, title: 'Kitchen', genre: 'Fiction', published: 1988 )
+Book.create(author_id: 2, title: 'Norwegian Wood', genre: 'Romance', published: 1987 )
+Book.create(author_id: 3, title: 'A Little Life', genre: 'Novel', published: 2015 )
+Book.create(author_id: 3, title: 'The People in the Trees', genre: 'Science Fiction', published: 2013 )
+Book.create(author_id: 4, title: 'Heaven', genre: 'Coming of Age', published: 2009 )
+Book.create(author_id: 5, title: 'My Brilliant Friend', genre: 'Novel', published: 2011 )
+Book.create(author_id: 5, title: 'The Lying Life of Adults', genre: 'Domestic Fiction', published: 2019 )
+
+2
