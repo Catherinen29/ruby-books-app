ceda5e64b6b6b14c8540d5e395adbcd22b9c366a
Catherine Nevin <cat_nevin@hotmail.com>
2023-05-23 21:26:47 +0100

    Create Author model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Author.create(first_name: 'Banana', last_name: 'Yoshimoto', number_of_books: 12, author_birthdate: 1964-07-24)
+Author.create(first_name: 'Haruki', last_name: 'Murakami', number_of_books: 28, author_birthdate: 1949-01-12)
+Author.create(first_name: 'Hanya', last_name: 'Yanagihara', number_of_books: 3, author_birthdate: 1974-01-01 )
+Author.create(first_name: 'Mieko', last_name: 'Kawakami', number_of_books: 13, author_birthdate: 1976-8-29 )
+Author.create(first_name: 'Elena', last_name: 'Ferrante', number_of_books: 12, author_birthdate: 1943-04-05)
